45174eeccfaaf92b972c856c075743117b7aa360
Lema hellali <lemahellali@Lemas-MacBook-Neo.local>
2026-06-11 13:33:36 -0400

    Update assignment 10 based on mentor feedback

--- save_to_database.py
+++ save_to_database.py
@@ -1,17 +1,28 @@
 import sqlite3
 import pandas as pd
 
-# Connect to SQLite database
+
 conn = sqlite3.connect("capstone_weather.db")
 
-# Read CSV files
+
 weather_df = pd.read_csv("weather_data.csv")
 
-# Save CSV data into SQLite table
-weather_df.to_sql("weather_data", conn, if_exists="replace", index=False)
 
-# Check if it worked
-print("Data saved to SQLite database successfully!")
+weather_df = weather_df.drop_duplicates()
 
-# Close connection
+weather_df.columns = weather_df.columns.str.strip()
+
+weather_df = weather_df.fillna("Unknown")
+
+
+weather_df.to_sql(
+    "weather_data",
+    conn,
+    if_exists="replace",
+    index=False
+)
+
+print("Cleaned data saved to SQLite database successfully!")
+
+
 conn.close()
